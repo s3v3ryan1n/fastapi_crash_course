64cc23ebedd4583baed2c983a67b2f2b186305a3
s3v3ryan1n <arsenii.burkov@gmail.com>
2025-07-18 15:45:37 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#10 занятие
+'''
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/users")
+async def get_users():
+    return [{"id" : 1, "name" : "Artem"}]
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0")
+
+'''
+#11 занятие
+
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/users")
+async def get_users():
+    return [{"id" : 1, "name" : "Artem"}]
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0")
